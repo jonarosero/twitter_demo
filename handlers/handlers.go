20196c1f64fd2790918b52395bde468e59b275e6
jonarosero <jars9605@gmail.com>
2021-09-09 08:38:45 -0500

    verifico Relacion

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -30,6 +30,7 @@ func Manejadores () {
 
 	router.HandleFunc("/altaRelacion", middlew.ChequeoBD(middlew.ValidoJWT(routers.AltaRelacion))).Methods("POST")
 	router.HandleFunc("/bajaRelacion", middlew.ChequeoBD(middlew.ValidoJWT(routers.BajaRelacion))).Methods("DELETE")
+	router.HandleFunc("/consultoRelacion", middlew.ChequeoBD(middlew.ValidoJWT(routers.ConsultoRelacion))).Methods("GET")
 
 	
 	PORT := os.Getenv("PORT")
---
+++ routers/consultaRelacion.go
@@ -0,0 +1,32 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jonarosero/twitter_demo/bd"
+	"github.com/jonarosero/twitter_demo/models"
+)
+
+func ConsultoRelacion (w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+
+	var t models.Relacion
+	t.UsuarioID = IDUsuario
+	t.UsuarioRelacionID = ID
+
+	var resp models.RespuestaConsultaRelacion
+
+	status, err := bd.ConsultoRelacion(t)
+
+	if err != nil || !status{
+		resp.Status=false
+	} else {
+		resp.Status=true
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(resp)
+}
