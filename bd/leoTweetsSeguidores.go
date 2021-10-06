745df4765e237e134fc3510d1d8ebaef2fb3a3da
jonarosero <jars9605@gmail.com>
2021-10-06 13:00:46 -0500

    correccion errores

--- bd/leoTweetsSeguidores.go
+++ bd/leoTweetsSeguidores.go
@@ -8,8 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func LeoTweetsSeguidores (ID string, pagina int) ([]models.DevuelvoTweetsSeguidores, bool) {
-
+func LeoTweetsSeguidores(ID string, pagina int) ([]models.DevuelvoTweetsSeguidores, bool) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
@@ -18,31 +17,25 @@ func LeoTweetsSeguidores (ID string, pagina int) ([]models.DevuelvoTweetsSeguido
 
 	skip := (pagina - 1) * 20
 
-	condiciones := make([]bson.M,0)
-
-	condiciones = append(condiciones, bson.M{"$match": bson.M{"usarioid":ID}})
-	condiciones = append(condiciones, bson.M{"$lookup": bson.M{
-		"from": "tweet",
-		"localfield": "usuariorelacionid",
-		"foreignField": "userid",
-		"as":"tweet",
-	}})
-
+	condiciones := make([]bson.M, 0)
+	condiciones = append(condiciones, bson.M{"$match": bson.M{"usuarioid": ID}})
+	condiciones = append(condiciones, bson.M{
+		"$lookup": bson.M{
+			"from":         "tweet",
+			"localField":   "usuariorelacionid",
+			"foreignField": "userid",
+			"as":           "tweet",
+		}})
 	condiciones = append(condiciones, bson.M{"$unwind": "$tweet"})
-	condiciones = append(condiciones, bson.M{"$sort": bson.M{"fecha": -1}})
+	condiciones = append(condiciones, bson.M{"$sort": bson.M{"tweet.fecha": -1}})
 	condiciones = append(condiciones, bson.M{"$skip": skip})
 	condiciones = append(condiciones, bson.M{"$limit": 20})
 
 	cursor, err := col.Aggregate(ctx, condiciones)
-
 	var result []models.DevuelvoTweetsSeguidores
-	
 	err = cursor.All(ctx, &result)
-
 	if err != nil {
 		return result, false
 	}
-
 	return result, true
-
 }
