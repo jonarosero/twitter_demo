658ad37d7e5f6fa0235b8e1ba132bc072d2c49c9
jonarosero <jars9605@gmail.com>
2021-10-13 10:17:38 -0500

    update modifico rol

--- bd/modificoRol.go
+++ bd/modificoRol.go
@@ -9,7 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ModificoRolUsuario (u models.Usuario, ID string) (bool, error) {
+func ModificoRolUsuario(u models.Usuario, ID string) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 
 	defer cancel()
@@ -17,16 +17,12 @@ func ModificoRolUsuario (u models.Usuario, ID string) (bool, error) {
 	db := MongoCN.Database("twittor")
 	col := db.Collection("usuarios")
 
-	registro := make(map[string]interface{})
-	
-	registro["rolid"] = u.RolId
-
 	updtString := bson.M{
-		"$set": registro,
+		"$set": u.RolId,
 	}
 
 	objID, _ := primitive.ObjectIDFromHex(ID)
-	filtro := bson.M{"_id": bson.M{"$eq":objID}}
+	filtro := bson.M{"_id": bson.M{"$eq": objID}}
 
 	_, err := col.UpdateOne(ctx, filtro, updtString)
 
